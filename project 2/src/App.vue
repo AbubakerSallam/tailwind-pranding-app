5f49fa6fadf038d8636b381fcc08354f9c7ed1d7
AbubakerSallam <abubaker4sallam@gmail.com>
2025-07-30 15:18:26 +0300

    complete site in project 2

--- project 2/src/App.vue
+++ project 2/src/App.vue
@@ -57,6 +57,117 @@
     </div>
     <div class="image"><img src="./assets/images/section-two.jpg" class="w-[500px]" /></div>
   </section>
+
+  <section class="mt-40">
+    <h1 class="ml-10 mb-10 text-4xl font-bold">Services</h1>
+    <div
+      class="md:h-[40rem] sm:h-auto md:w-60 sm:w-auto items-center bg-[#292A2B] flex md:flex-nowrap sm:flex-wrap"
+    >
+      <div class="one">
+        <div class="relative w-[300px] ml-10 mb-10 bg-[#1BC8D3] left-20 top-10 p-10">
+          <h1 class="font-bold mb-10">
+            Web <br />
+            Development
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro quibusdam
+            animi blanditiis veritatis optio totam corporis aperiam
+          </p>
+        </div>
+        <div class="relative w-[300px] ml-10 mb-10 bg-[#1BC8D3] left-20 top-10 p-10">
+          <h1 class="font-bold mb-10">
+            Web <br />
+            Design
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro quibusdam
+            animi blanditiis veritatis optio totam corporis aperiam
+          </p>
+        </div>
+      </div>
+      <div class="tow">
+        <div class="relative w-[300px] ml-10 mb-10 bg-[#292A2B] text-white left-20 top-10 p-10">
+          <h1 class="font-bold mb-10">Analytic</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro quibusdam
+            animi blanditiis veritatis optio totam corporis aperiam
+          </p>
+        </div>
+        <div class="relative w-[300px] ml-10 mb-10 bg-[#292A2B] text-white left-20 top-10 p-10">
+          <h1 class="font-bold mb-10">
+            Seo <br />
+            Smm
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro quibusdam
+            animi blanditiis veritatis optio totam corporis aperiam
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="mt-32">
+    <h1 class="mr-20 text-right text-5xl font-bold mb-10">Recent Projects</h1>
+    <div class="m-20 flex justify-around items-center flex-wrap mx-auto">
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+      <div class="w-64 m-10">
+        <img src="./assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" />
+        <div class="bg-[#1BC8D3] text-white p-2">Web Design</div>
+      </div>
+    </div>
+  </section>
+  <footer class="bg-[#292A2B] py-10">
+    <div class="flex justify-around items-center flex-wrap">
+      <div class="card">
+        <h1 class="font-bold mb-5 text-white">SITE MENUE</h1>
+        <p class="text-white">About Us</p>
+        <p class="text-white">Photography</p>
+        <p class="text-white">Lens Index</p>
+      </div>
+      <div class="card">
+        <h1 class="font-bold mb-5 text-white">REVIEW</h1>
+        <p class="text-white">Reviews Archive</p>
+        <p class="text-white">Camera Reviews</p>
+        <p class="text-white">Lens Reviews</p>
+        <p class="text-white">Other Gear Reviews</p>
+      </div>
+      <div class="card">
+        <h1 class="font-bold mb-5 text-white">More</h1>
+        <p class="text-white">Contact Us</p>
+        <p class="text-white">Subscribe</p>
+        <p class="text-white">Workshops</p>
+        <p class="text-white">Support Us</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup></script>
